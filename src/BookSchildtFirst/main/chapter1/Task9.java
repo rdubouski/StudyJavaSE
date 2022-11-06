32cab3f62ac6fcf9826ed8fbd23e705507722ae5
rdubouski <renics108>
2022-11-06 12:09:51 +0300

    Initial

---
+++ src/BookSchildtFirst/main/chapter1/Task9.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+public class Task9 {
+    public static void main(String[] args) {
+
+        double weight, weightomoon;
+
+        weight = 115;
+        weightomoon = weight * 0.17;
+        System.out.println(weight + " weight in earth = " + weightomoon + " weight in moon");
+    }
+}
