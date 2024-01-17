e4cb8acb13123b0d6984e2f575c11d65e99dd6ba
Raman <rdubouski@gmail.com>
2024-01-17 22:37:33 +0300

    Day 3 complete

--- src/JavaMarathon2020/main/java/day3/Task2.java
+++ src/JavaMarathon2020/main/java/day3/Task2.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        double a, b;
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            a = scanner.nextDouble();
+            b = scanner.nextDouble();
+            if (b == 0) return;
+            System.out.println(a / b);
+        }
     }
 }
--- src/JavaMarathon2020/main/java/day3/Task3.java
+++ src/JavaMarathon2020/main/java/day3/Task3.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        double a, b;
+        int i = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (i < 5){
+            a = scanner.nextDouble();
+            b = scanner.nextDouble();
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                i++;
+                continue;
+            }
+            System.out.println(a / b);
+            i++;
+        }
     }
 }
