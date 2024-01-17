e6e168c0860bbbc568086c824328af049b0f66d3
Raman <rdubouski@gmail.com>
2024-01-17 22:16:25 +0300

    Day 2 complete

--- src/JavaMarathon2020/main/java/day2/Task1.java
+++ src/JavaMarathon2020/main/java/day2/Task1.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        if (x >= 1 && x <= 4) System.out.println("Малоэтажный дом");
+        if (x >= 5 && x <= 8) System.out.println("Среднеэтажный дом");
+        if (x >= 9) System.out.println("Многоэтажный дом");
+        if (x <= 1) System.out.println("Ошибка ввода");
     }
 }
--- src/JavaMarathon2020/main/java/day2/Task3.java
+++ src/JavaMarathon2020/main/java/day2/Task3.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if (a >= b) {
+            System.out.println("Error");
+        } else {
+            while (a < b){
+                if ((a % 5 == 0) && (a % 10 != 0)){
+                    System.out.print(a + " ");
+                }
+                a++;
+            }
+        }
     }
 }
