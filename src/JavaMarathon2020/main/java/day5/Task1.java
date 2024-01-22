30f1bbdc5348f4aa14bb30b9026ba31798b0373b
Raman <rdubouski@gmail.com>
2024-01-22 23:05:47 +0300

    Day 5 complete

--- src/JavaMarathon2020/main/java/day5/Task1.java
+++ src/JavaMarathon2020/main/java/day5/Task1.java
@@ -2,6 +2,45 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car lada = new Car();
+        lada.setModel("Lada");
+        lada.setColor("Black");
+        lada.setYear(2019);
+
+        System.out.println("Model " + lada.getModel());
+        System.out.println("Color " + lada.getColor());
+        System.out.println("Year " + lada.getYear());
 
     }
 }
+
+class Car {
+    private int year;
+    private String model;
+    private String color;
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
+
--- src/JavaMarathon2020/main/java/day5/Task2.java
+++ src/JavaMarathon2020/main/java/day5/Task2.java
@@ -2,6 +2,48 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Moto java = new Moto("Java", "White", 2019);
+
+        System.out.println("Model " + java.getModel());
+        System.out.println("Color " + java.getColor());
+        System.out.println("Year " + java.getYear());
 
     }
 }
+
+class Moto {
+    private int year;
+    private String model;
+    private String color;
+
+    Moto(String model, String color, int year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
+
