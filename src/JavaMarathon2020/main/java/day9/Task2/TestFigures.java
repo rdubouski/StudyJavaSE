abfa3bc09a629954a8a648c3617928a8bd4ab77a
Raman <rdubouski@gmail.com>
2024-04-08 22:53:46 +0300

    Day 9 complete

--- src/JavaMarathon2020/main/java/day9/Task2/TestFigures.java
+++ src/JavaMarathon2020/main/java/day9/Task2/TestFigures.java
@@ -2,6 +2,38 @@ package day9.Task2;
 
 public class TestFigures {
     public static void main(String[] args) {
+        Figure[] figures = {
+                new Triangle(10, 10, 10, "Red"),
+                new Triangle(10, 20, 30, "Green"),
+                new Triangle(10, 20, 15, "Red"),
+                new Rectangle(5, 10, "Red"),
+                new Rectangle(40, 15, "Orange"),
+                new Circle(4, "Red"),
+                new Circle(10, "Red"),
+                new Circle(5, "Blue"),
+        };
 
+        System.out.println(calculateRedArea(figures));
+        System.out.println(calculateRedPerimeter(figures));
+    }
+
+    public static double calculateRedPerimeter(Figure[] figures) {
+        double sum = 0;
+        for (Figure f : figures) {
+            if (f.getColor().equals("Red")) {
+                sum = sum + f.perimeter();
+            }
+        }
+        return sum;
+    }
+
+    public static double calculateRedArea(Figure[] figures) {
+        double sum = 0;
+        for (Figure f : figures) {
+            if (f.getColor().equals("Red")) {
+                sum = sum + f.area();
+            }
+        }
+        return sum;
     }
 }
