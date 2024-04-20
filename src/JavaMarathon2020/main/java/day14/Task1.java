9b43687ed149498ad2d21b230b09fa7d0685c227
Raman <rdubouski@gmail.com>
2024-04-20 19:31:48 +0300

    Day 14 complete

--- src/JavaMarathon2020/main/java/day14/Task1.java
+++ src/JavaMarathon2020/main/java/day14/Task1.java
@@ -1,7 +1,43 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        String path = "./txt/JavaMarathon2020/day14/task1.txt";
+
+        File file = new File(path);
+
+        printSumDigits(file);
+
+    }
+
+    public static void printSumDigits(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            String line = scanner.nextLine();
+            String[] strings = line.split(" ");
+            if (strings.length != 10) {
+                throw new IllegalArgumentException();
+            }
+            int sum = 0;
+
+            for (String string : strings) {
+                sum += Integer.parseInt(string);
+            }
+
+            System.out.println(sum);
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
     }
 }
--- src/JavaMarathon2020/main/java/day14/Task3.java
+++ src/JavaMarathon2020/main/java/day14/Task3.java
@@ -1,7 +1,44 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
 
+        String path = "./txt/JavaMarathon2020/day14/people.txt";
+
+        File file = new File(path);
+
+        System.out.println(parseFileToObjList(file));
+
     }
+
+    public static List<Person> parseFileToObjList(File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+            List<Person> people = new ArrayList<>();
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] person = line.split(" ");
+                if (Integer.parseInt(person[1]) < 0) {
+                    throw new IllegalArgumentException();
+                }
+                people.add(new Person(person[0], Integer.parseInt(person[1])));
+            }
+
+            return people;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
+        return null;
+    }
+
 }
+
