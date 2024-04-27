6ef581492b036a3b5b70b2fe4fd714ef2208cfc0
Raman <rdubouski@gmail.com>
2024-04-27 19:56:37 +0300

    empty field final day

--- src/JavaMarathon2020/main/java/finalday/GameField.java
+++ src/JavaMarathon2020/main/java/finalday/GameField.java
@@ -2,6 +2,30 @@ package finalday;
 
 public class GameField {
 
+    private String[][] field;
+    private static final String[][] DEFAULTFIELD = {
+            {"O", "O", "O", "O", "O", "O", "O", "O", "O", "O"},
+            {"O", "O", "O", "O", "O", "O", "O", "O", "O", "O"},
+            {"O", "O", "O", "O", "O", "O", "O", "O", "O", "O"},
+            {"O", "O", "O", "O", "O", "O", "O", "O", "O", "O"},
+            {"O", "O", "O", "O", "O", "O", "O", "O", "O", "O"},
+            {"O", "O", "O", "O", "O", "O", "O", "O", "O", "O"},
+            {"O", "O", "O", "O", "O", "O", "O", "O", "O", "O"},
+            {"O", "O", "O", "O", "O", "O", "O", "O", "O", "O"},
+            {"O", "O", "O", "O", "O", "O", "O", "O", "O", "O"},
+            {"O", "O", "O", "O", "O", "O", "O", "O", "O", "O"}
+    };
 
+    public GameField() {
+        this.field = DEFAULTFIELD;
+    }
 
+    public void printField() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(this.field[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
--- src/JavaMarathon2020/main/java/finalday/SeaBattle.java
+++ src/JavaMarathon2020/main/java/finalday/SeaBattle.java
@@ -1,5 +1,8 @@
 package finalday;
 
 public class SeaBattle {
-
+    public static void main(String[] args) {
+        GameField gameField1 = new GameField();
+        gameField1.printField();
+    }
 }
