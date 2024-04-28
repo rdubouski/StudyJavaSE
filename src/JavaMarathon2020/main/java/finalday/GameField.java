a0b19ce88a66ab4c7977aa41993a923ddebd2b62
Raman <rdubouski@gmail.com>
2024-04-28 10:09:00 +0300

    final day. place 0.1

--- src/JavaMarathon2020/main/java/finalday/GameField.java
+++ src/JavaMarathon2020/main/java/finalday/GameField.java
@@ -1,5 +1,7 @@
 package finalday;
 
+import java.util.Scanner;
+
 public class GameField {
 
     private String name;
@@ -26,7 +28,9 @@ public class GameField {
 
     public void printField(String name) {
         System.out.println("Поле игрока " + name);
+        System.out.println(" 0123456789");
         for (int i = 0; i < 10; i++) {
+            System.out.print(i);
             for (int j = 0; j < 10; j++) {
                 System.out.print(this.myfield[i][j]);
             }
@@ -39,6 +43,33 @@ public class GameField {
                 System.out.print(this.enemyfield[i][j]);
             }
             System.out.println();
+        }
+    }
+
+    public void place4Ship() {
+        boolean check = false;
+        Scanner scanner;
+        String line;
+        String[] coordinateAll;
+        int[][] coordinates = new int[4][2];
+
+        while (!check) {
+            int i = 0, j = 0;
+            System.out.println("Введи координаты четырехпалубного корабля (формат: x,y;x,y;x,y;x,y)");
+            scanner = new Scanner(System.in);
+            line = scanner.nextLine();
+            coordinateAll = line.split(";");
+            for (String string : coordinateAll) {
+                coordinates[i][j] = Integer.parseInt(String.valueOf(string.charAt(0)));
+                coordinates[i][j + 1] = Integer.parseInt(String.valueOf(string.charAt(2)));
+                i++;
+            }
+
+            for (i = 0; i < 4; i++) {
+                this.myfield[coordinates[i][0]][coordinates[i][1]] = "o";
+            }
+
+            check = true;
         }
     }
 }
--- src/JavaMarathon2020/main/java/finalday/SeaBattle.java
+++ src/JavaMarathon2020/main/java/finalday/SeaBattle.java
@@ -9,6 +9,16 @@ public class SeaBattle {
         String player1 = scanner.nextLine();
         GameField gameField1 = new GameField(player1);
         System.out.print("\033[H\033[J");
+
+        System.out.print("Введите имя второго игрока: ");
+        scanner = new Scanner(System.in);
+        String player2 = scanner.nextLine();
+        GameField gameField2 = new GameField(player2);
+        System.out.print("\033[H\033[J");
+
+        gameField1.printField(player1);
+        System.out.println("Начнем расставлять корабли на поле " + player1 + ". Другой игрок не смотри!");
+        gameField1.place4Ship();
         gameField1.printField(player1);
     }
 }
