580f5f14edfeed6112d241f63314b855bb1e7209
Raman <rdubouski@gmail.com>
2024-04-28 17:17:04 +0300

    final day. place 4ship 0.2

--- src/JavaMarathon2020/main/java/finalday/GameField.java
+++ src/JavaMarathon2020/main/java/finalday/GameField.java
@@ -7,7 +7,20 @@ public class GameField {
     private String name;
     private String[][] myfield;
     private String[][] enemyfield;
-    private static final String[][] DEFAULTFIELD = {
+    private static final String[][] DEFAULTFIELDMY = {
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
+            {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"}
+    };
+
+    private static final String[][] DEFAULTFIELDENEMY = {
             {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
             {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
             {"-", "-", "-", "-", "-", "-", "-", "-", "-", "-"},
@@ -22,15 +35,15 @@ public class GameField {
 
     public GameField(String name) {
         this.name = name;
-        this.myfield = DEFAULTFIELD;
-        this.enemyfield = DEFAULTFIELD;
+        this.myfield = DEFAULTFIELDMY;
+        this.enemyfield = DEFAULTFIELDENEMY;
     }
 
     public void printField(String name) {
         System.out.println("Поле игрока " + name);
-        System.out.println(" 0123456789");
+        System.out.println("  0123456789");
         for (int i = 0; i < 10; i++) {
-            System.out.print(i);
+            System.out.print(i + " ");
             for (int j = 0; j < 10; j++) {
                 System.out.print(this.myfield[i][j]);
             }
@@ -38,7 +51,9 @@ public class GameField {
         }
 
         System.out.println("Поле врага");
+        System.out.println("  0123456789");
         for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
             for (int j = 0; j < 10; j++) {
                 System.out.print(this.enemyfield[i][j]);
             }
@@ -55,9 +70,13 @@ public class GameField {
 
         while (!check) {
             int i = 0, j = 0;
-            System.out.println("Введи координаты четырехпалубного корабля (формат: x,y;x,y;x,y;x,y)");
+            System.out.println("Введи координаты четырехпалубного корабля (от 0 до 9) (формат: x,y;x,y;x,y;x,y)");
             scanner = new Scanner(System.in);
             line = scanner.nextLine();
+            if (line.length() != 15) {
+                System.out.println("Неверное количество координат!");
+                continue;
+            }
             coordinateAll = line.split(";");
             for (String string : coordinateAll) {
                 coordinates[i][j] = Integer.parseInt(String.valueOf(string.charAt(0)));
@@ -66,10 +85,14 @@ public class GameField {
             }
 
             for (i = 0; i < 4; i++) {
-                this.myfield[coordinates[i][0]][coordinates[i][1]] = "o";
+                this.printCoordinate(coordinates[i][0], coordinates[i][1], "o");
             }
 
             check = true;
         }
+    }
+
+    public void printCoordinate(int x, int y, String value) {
+        this.myfield[x][y] = value;
     }
 }
